4b501e40aa902e5a2bb773886c26b4e3818bd2f6
Macarse <macarse@gmail.com>
2020-12-24 18:04:08 -0600

    fixed: PVE003

--- tests/development/test_migration.py
+++ tests/development/test_migration.py
@@ -38,3 +38,44 @@ def test_migration(
     assert hegic.balanceOf(strategy2) == Wei("12000 ether")
 
     assert hegic.balanceOf(vault) == 0
+
+
+def test_migration_missing_profit(
+    gov, hegic, hegicStaking, vault, strategy, strategist, bob, alice, strategy2
+):
+
+    hegic.approve(gov, 2 ** 256 - 1, {"from": gov})
+    hegic.transferFrom(gov, bob, Wei("800000 ether"))
+    hegic.transferFrom(gov, alice, Wei("100000 ether"))
+
+    vault.setManagementFee(0)
+    hegic.approve(vault, 2 ** 256 - 1, {"from": bob})
+    hegic.approve(vault, 2 ** 256 - 1, {"from": alice})
+    vault.deposit(hegic.balanceOf(bob), {"from": bob})
+    vault.deposit(hegic.balanceOf(alice), {"from": alice})
+    vault.addStrategy(strategy, Wei("1776000 ether"), 0, 0, {"from": gov})
+
+    strategy.harvest()
+    assert hegicStaking.balanceOf(strategy) == 1
+    assert hegic.balanceOf(strategy) == Wei("12000 ether")
+    assert hegic.balanceOf(vault) == 0
+
+    # Send profit
+    hegicStaking.sendProfit({"value": Wei("1 ether")})
+    assert hegicStaking.balance() == Wei("1 ether")
+    assert strategy.hegicFutureProfit() > 0
+
+    vault.migrateStrategy(strategy, strategy2, {"from": gov})
+
+    assert hegicStaking.balanceOf(strategy) == 0
+    assert hegic.balanceOf(strategy) == 0
+    assert hegicStaking.balanceOf(strategy2) == 1
+    assert hegic.balanceOf(strategy2) == Wei("12000 ether")
+
+    # a harvest would revert since the strategy is not longer added to the vault
+    with brownie.reverts():
+        strategy.harvest()
+
+    vault.addStrategy(strategy, 0, 0, 0, {"from": gov})
+    strategy.harvest()
+    assert strategy.hegicFutureProfit() == 0
